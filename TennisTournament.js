7812b4c90fc1ec6c9d4fcd848526538b440c1e14
Vishnu Varthanan T S <vishnumastro5101@gmail.com>
2022-09-05 17:31:02 +0530

    I added Tennis Tournament Program

--- TennisTournament.js
+++ TennisTournament.js
@@ -1,41 +1,33 @@
-
-tournament = [{ TeamID: "MI", Team: "Mumbai", Rank: 1 },
-{ TeamID: "CSK", Team: "Chennai", Rank: 2 },
-{ TeamID: "KKR", Team: "Kolkata", Rank: 3 },
-{ TeamID: "DC", Team: "Delhi", Rank: 4 },
-{ TeamID: "RR", Team: "Rajasthan", Rank: 5 },
-{ TeamID: "SRH", Team: "Hyderabad", Rank: 6 },
-{ TeamID: "GT", Team: "Gujarat", Rank: 7 },
-{ TeamID: "PK", Team: "Punjab", Rank: 8 },
-{ TeamID: "MI", Team: "haryana", Rank: 9 },
-{ TeamID: "CSK", Team: "pune", Rank: 10 },
-{ TeamID: "KKR", Team: "Kerala", Rank: 11 },
-{ TeamID: "DC", Team: "nepal", Rank: 12 },
-{ TeamID: "RR", Team: "assam", Rank: 13 },
-{ TeamID: "SRH", Team: "bihar", Rank: 14 },
-{ TeamID: "GT", Team: "tamilnadu", Rank: 15 },
-{ TeamID: "PK", Team: "andhra", Rank: 16 }]
-function probability(team1Rank,team2Rank){
-    lo=Math.random().toFixed(2)
-    x =0,y=0
-    if(lo>0.5){
-        x =lo
-        y=(1-x).toFixed(2)
-    }else{
-        y=lo
-        x=(1-y).toFixed(2)
+// Player Names and Details
+tournament = [{ PlayerID: "RF1", Team: "Roger Federer", Rank: 1 },
+{ PlayerID: "RN2", Team: "Rafael Nadal", Rank: 2 },
+{ PlayerID: "SW3", Team: "Serena Williams", Rank: 3 },
+{ PlayerID: "VW4", Team: "Venus Williams", Rank: 4 },
+{ PlayerID: "ND5", Team: "Novak Djokovic", Rank: 5 },
+{ PlayerID: "FS6", Team: "Frank Sedgman", Rank: 6 },
+{ PlayerID: "SS7", Team: "Stan Smith", Rank: 7 },
+{ PlayerID: "Sk8", Team: "Svetlana Kuznetsova", Rank: 8 }]
+// Winning Probability function By Player Rank
+function probability(t1, t2) {
+    a = findingRankByUsingName(t1)
+    b = findingRankByUsingName(t2)
+    c = Math.round(100 / (Math.abs(a - b) + 1))
+    b = c
+    a = 100 - b
+    probList = []
+    for (d = 1; d <= 100; d++) {
+        if (d <= a) {
+            probList.push(t1)
+        }
+        else {
+            probList.push(t2)
+        }
     }
-    
-    console.log("Rank 1:",x)
-    console.log("Rank 8:",y)
-    // j=[x,x-(0.1*x),x-(0.2*x),x-]
-    if(team1Rank<team2Rank & x>=0.45){
-        return team1Rank
-    }
-    else {
-        return team2Rank
-    }
+    ran = Math.floor(Math.random() * 100)
+    a=probList[ran]
+    return a
 }
+// Initial Match Schedule
 match = []
 for (i = 0; i < tournament.length / 2; i++) {
     match.push([tournament[i].Team, tournament[tournament.length - 1 - i].Team])
@@ -54,27 +46,29 @@ for (i in match) {
 for (i of poolB.reverse()) {
     poolA.push(i)
 }
+z = 0
+// Scheduling the Matches
 winners = []
 function winner() {
     poolAWinners = []
     winnerslist = []
     for (i = 0; i < poolA.length; i = i + 2) {
         l = []
-        d = i + 1
+        v = i + 1
         e = i + 2
         ob1 = {}
         ob2 = {}
-        a = (poolA[i][Math.floor(Math.random() * 2)])
-        ob1["MatchNo"] = d
+        a = probability(poolA[i][0], poolA[i][1])
+        ob1["MatchNo"] = v
         ob1["Match"] = poolA[i][0] + " vs " + poolA[i][1]
         ob1["MatchId"] = poolA[i][0].toLowerCase().slice(0, 2) + findingRankByUsingName(poolA[i][0]) + poolA[i][1].toLowerCase().slice(0, 2) + findingRankByUsingName(poolA[i][1])
         ob1["Winner"] = a
-        b = (poolA[i + 1][Math.floor(Math.random() * 2)])
+        l.push(a)
+        b = probability(poolA[i + 1][0], poolA[i + 1][1])
         ob2["MatchNo"] = e
         ob2["Match"] = poolA[i + 1][0] + " vs " + poolA[i + 1][1]
         ob2["MatchId"] = poolA[i + 1][0].toLowerCase().slice(0, 2) + findingRankByUsingName(poolA[i + 1][0]) + poolA[i + 1][1].toLowerCase().slice(0, 2) + findingRankByUsingName(poolA[i + 1][1])
         ob2["Winner"] = b
-        l.push(a)
         l.push(b)
         poolAWinners.push(l)
         winnerslist.push(ob1)
@@ -88,8 +82,11 @@ function winner() {
     if (poolAWinners.length == 1) {
         return poolAWinners
     }
+    
 }
+// Final Round
 finalRound = winner()
+// Finding Rank by Player Name
 function findingRankByUsingName(teamName) {
     rank = 0;
     tournament.forEach(element => {
@@ -99,6 +96,7 @@ function findingRankByUsingName(teamName) {
     });
     return rank
 }
+// Final Match
 final = {}
 final["MatchNo"] = "Final"
 final["Match"] = finalRound[0][0] + " vs " + finalRound[0][1]
@@ -107,8 +105,37 @@ final["Winner"] = finalRound[0][Math.floor(Math.random() * 2)]
 winners.push([final])
 count = 1
 winners.forEach(element => {
+    console.log("----------------------------------------------------------------------------------------")
     console.log("Round", count)
     console.table(element)
+    console.log("----------------------------------------------------------------------------------------")
     count++
 });
+// Finding Match Details by Match ID
+function findMatchDetailsbyMatchId(str){
+    winners.forEach(round => {
+        for( i of round){
+            if(i.MatchId==str){
+                console.log("----------------------------------------------------------------------------------------")
+                console.log(`Match Details for "${str}" MatchID`)
+                console.table([i])
+                console.log("----------------------------------------------------------------------------------------")
+            }
+        }        
+    });
+}
+findMatchDetailsbyMatchId("se3fr6")
+// Finding Player Details by PlayerID
+function findPlayerDetailsbyPlayerId(str){
+    tournament.forEach(element => {
+        if(element.PlayerID==str){
+            console.log("----------------------------------------------------------------------------------------")
+            console.log(`Player Details for "${str}" PlayerID`)
+            console.table([element])
+            console.log("----------------------------------------------------------------------------------------")
+        }
+    });
+}
+findPlayerDetailsbyPlayerId("FS6")
+
 
